7ec7cf92045d2c35b04bc2b6768b6da093975deb
EmilioVS <6360@holbertonstudents.com>
2023-10-13 11:57:34 -0600

    proyecto3

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+function displayMessage(message) {
+    if (message) {
+      process.stdout.write('Hello Holberton School\n');
+      process.stdout.write('We are going to learn Node JS today\n');
+    }
+  }
+  module.exports = displayMessage;
